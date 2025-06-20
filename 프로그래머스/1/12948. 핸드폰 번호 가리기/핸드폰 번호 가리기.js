75212478720134a53b0c38ab768fdf1b64be5f06
Park yejin <yerealuu@gmail.com>
2025-06-20 20:14:53 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -1,5 +1,15 @@
 function solution(phone_number) {
     
-    return phone_number.replace(/\d(?=\d{4})/g,'*');
+    const last_number = phone_number.slice(-4);
+    
+    let answer='';
+    
+    for(i=0;i<phone_number.length-4;i++) {
+        answer += '*';
+    }
+    
+    answer += last_number;
+
+    return answer;
     
 }
